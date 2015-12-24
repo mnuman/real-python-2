cd21637bfd0c6e86958c1cf0ac2da32fab2dd337
Milco Numan <milco.numan@gmail.com>
2015-12-24 20:02:35 +0100

    Finished chapter 5 - basic flask application with static and dynamic routing

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -1,9 +1,11 @@
-# --- Flask Hello World --- #
-
+#
+#--- Flask Hello World --- #
 # import the Flask class from the flask package
 from flask import Flask
 # create the application object
 app = Flask(__name__)
+# Error Handling
+app.config["DEBUG"] = True
 
 
 # use decorators to link the function to the URL
@@ -12,7 +14,39 @@ app = Flask(__name__)
 @app.route("/renefroger")
 # define the view using a function which returns a string
 def hello_world():
-    return "Hello, world!"
+    return "Hello, World!?!?!?!?"
+
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1
+    return "correct"
+
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "milco":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 if __name__ == "__main__":
     app.run()
