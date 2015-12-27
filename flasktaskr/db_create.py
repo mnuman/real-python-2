00ad20c1edf7c59c1b26ccd7771066f0bcbbc986
Milco Numan <milco.numan@gmail.com>
2015-12-27 19:57:05 +0100

    Moved from direct SQL to SQLAlchemy ORM

--- flasktaskr/db_create.py
+++ flasktaskr/db_create.py
@@ -1,14 +1,10 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    # get a curspr object to execute SQL commands
-    c = connection.cursor()
 
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
-    # insert dummy data into table
-    c.execute('INSERT INTO tasks(name, due_date, priority, status)'
-              'values("Finish tutorial", "03/25/2015", 10, 1)')
-    c.execute('INSERT INTO tasks(name, due_date, priority, status)'
-              'values("Finish Real Python - 2 cource", "03/25/2015", 10, 1)')
+# create the database and the table
+db.create_all()
+db.session.add(Task("Finish this tutorial",date(2015,3,13), 10, 1))
+db.session.add(Task("Finish RealPython",date(2015,3,13), 10, 1))
+db.session.commit()
